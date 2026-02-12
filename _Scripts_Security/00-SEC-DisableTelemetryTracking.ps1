328453dfed39ad7ae65220ebdaeefb641c486cc5
xShadowDeveloper <106242550+xShadowDeveloper@users.noreply.github.com>
2026-02-13 00:20:27 +0100

    Sync upload _Scripts_Security/00-SEC-DisableTelemetryTracking.ps1

---
+++ _Scripts_Security/00-SEC-DisableTelemetryTracking.ps1
@@ -0,0 +1,17 @@
+# Disable Windows Telemetry
+#----------------------------------------
+# Requires Administrator previlegues
+
+$isadm = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if (-not $isadm) 
+{
+    Write-Host "<WRITE-LOG = ""*Please run this script as Administrator.*"">"
+    Write-Host "<WRITE-LOG = ""*If this was a remote execution please provide Administrator credentials.*"">"
+    Write-Error "Warning: Not running as Administrator."
+}
+else {
+	Stop-Service DiagTrack -Force
+	Set-Service DiagTrack -StartupType Disabled
+	Write-Host "Windows telemetry disabled!"
+}
+
