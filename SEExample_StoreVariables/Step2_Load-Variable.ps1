d2707ca16fe722295183420a22e0ddcdccb72a75
xShadowDeveloper <106242550+xShadowDeveloper@users.noreply.github.com>
2026-02-13 00:20:44 +0100

    -> Delete missing file SEExample_StoreVariables/Step2_Load-Variable.ps1

--- SEExample_StoreVariables/Step2_Load-Variable.ps1
+++
@@ -1,9 +0,0 @@
-# To access the stored variables
-# load it like this: $message = $store
-
-#load variable from $store
-$message = $store
-
-#output loaded variable in logs
-Write-Host "<WRITE-LOG = ""*load: $message*"">"
-write-host $message
