596f9a07422904f1776829ec306cb3b8de89991a
xShadowDeveloper <106242550+xShadowDeveloper@users.noreply.github.com>
2026-02-13 00:20:31 +0100

    Sync upload _Scripts_ServerEngine/02-SE-LoadData.ps1

---
+++ _Scripts_ServerEngine/02-SE-LoadData.ps1
@@ -0,0 +1,23 @@
+# To access and load stored data from the script before
+# use the $store variable like this:
+#--------------------------------------------------
+
+# Example 1 (Load Text)
+$example1 = $store
+
+# Example 2 (Save Variable)
+$example2 = $store
+
+# Example 3 (Save Numeric Values)
+$example3 = $store
+
+# Example 4 (Save Mixed Data)
+
+$Name = 	$store[0]
+$Age = 	$store[1]
+$Title =	$store[2]
+$Salary =	$store[3]
+
+#--------------------------------------------------
+# Only use one example at a time 
+#--------------------------------------------------
