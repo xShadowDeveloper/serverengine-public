2752305c5e709a7c85a4cab9ee9e6b0f962aaffd
xShadowDeveloper <106242550+xShadowDeveloper@users.noreply.github.com>
2026-02-13 00:20:21 +0100

    Sync upload _Scripts_Health/00-HEAL-ClearWindowsUpdate.ps1

---
+++ _Scripts_Health/00-HEAL-ClearWindowsUpdate.ps1
@@ -0,0 +1,17 @@
+# Clear the Windows Update cache
+#-------------------------------------------------
+
+$isadm = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if (-not $isadm) 
+{
+    Write-Host "<WRITE-LOG = ""*Please run this script as Administrator.*"">"
+    Write-Host "<WRITE-LOG = ""*If this was a remote execution please provide Administrator credentials.*"">"
+    Write-Error "Warning: Not running as Administrator."
+}
+else {
+	Stop-Service wuauserv -Force
+	Remove-Item -Path "C:\Windows\SoftwareDistribution" -Recurse -Force
+	Start-Service wuauserv
+	Write-Host "Windows Update cache cleared!"
+}
+
