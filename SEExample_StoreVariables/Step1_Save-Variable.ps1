554789df4ccb6ad789918b889cc3f87a80de597b
xShadowDeveloper <106242550+xShadowDeveloper@users.noreply.github.com>
2026-02-13 00:20:37 +0100

    -> Delete missing file SEExample_StoreVariables/Step1_Save-Variable.ps1

--- SEExample_StoreVariables/Step1_Save-Variable.ps1
+++
@@ -1,9 +0,0 @@
-# You can parse variables to the next script 
-# to save your variable use : $store = $message or $store = "hi" or $store = 154
-#
-# Example
-$message = "nice"
-Write-Host "<WRITE-LOG = ""*save: $message*"">"
-
-#save variable to store
-$store = $message
