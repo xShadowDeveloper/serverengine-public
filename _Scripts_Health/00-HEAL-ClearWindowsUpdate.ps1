0c8df8811d6dca2ed83ba68bf98e256e5fb12d53
xShadowDeveloper <106242550+xShadowDeveloper@users.noreply.github.com>
2026-02-17 20:13:18 +0100

    Sync upload _Scripts_Health/00-HEAL-ClearWindowsUpdate.ps1

--- _Scripts_Health/00-HEAL-ClearWindowsUpdate.ps1
+++ _Scripts_Health/00-HEAL-ClearWindowsUpdate.ps1
@@ -2,13 +2,12 @@
 #-------------------------------------------------
 
 $isadm = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-if (-not $isadm) 
-{
+if (-not $isadm) {
     Write-Host "<WRITE-LOG = ""*Please run this script as Administrator.*"">"
+    Show-Warning "Please run this script as Administrator."
     Write-Host "<WRITE-LOG = ""*If this was a remote execution please provide Administrator credentials.*"">"
     Write-Error "Warning: Not running as Administrator."
-}
-else {
+} else {
 	Stop-Service wuauserv -Force
 	Remove-Item -Path "C:\Windows\SoftwareDistribution" -Recurse -Force
 	Start-Service wuauserv
