cfc0d9b50b997927417fc6afdacb02f9a469431d
xShadowDeveloper <106242550+xShadowDeveloper@users.noreply.github.com>
2026-04-11 23:35:52 +0200

    -> ServerEngine Fullsync

---
+++ Reboot-Server.runbook/01-SE-ManagedReboot.ps1
@@ -0,0 +1,10 @@
+# ServerEngine can handle system reboot cycles 
+# and seemlesy continue the next scripts when using Runbooks. 
+# ServerEngine initiates a waiting sequence and after
+# successfull reconnection the cycle ends
+# IMPORTANT: Requires a script step after reboot
+#-------------------------------------------------
+
+# Use this specific line for successfully reboot cycles
+Invoke-Command { Start-Sleep 3;Write-Host " ""State : Reboot"" ";Restart-Computer -Force }; Exit-PSSession 
+
