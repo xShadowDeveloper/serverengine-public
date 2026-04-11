95d2dcf03d37260b6680613bf80ba28ded6cc6e3
xShadowDeveloper <106242550+xShadowDeveloper@users.noreply.github.com>
2026-04-11 23:36:04 +0200

    -> ServerEngine Fullsync

--- _Scripts_Checks/01-CHECK-InstalledSoftware.ps1
+++ _Scripts_Checks/01-CHECK-InstalledSoftware.ps1
@@ -1,13 +1,26 @@
 # Discover Installed Applications
 #-------------------------------------------
+function Write-Log {
+    param($Message)
+    Write-Host "<WRITE-LOG = `"*$Message*`">"
+}
 
 # Get Installed Programs (Win32)
 Write-Host "`n=== Installed Win32 Programs (Control Panel) ===" -ForegroundColor Cyan
-Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | 
-Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Sort-Object DisplayName | Format-Table -AutoSize
+$logListWin32 = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | 
+    Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Sort-Object DisplayName
+
+foreach ($log in $logListWin32) {
+    Write-Log $log
+}
+$logListWin32 | Format-Table -AutoSize
 
 # Get Installed Microsoft Store (UWP) Apps
 Write-Host "`n=== Installed Microsoft Store (UWP) Apps ===" -ForegroundColor Cyan
-Get-AppxPackage | 
-Select-Object Name, PackageFullName, Version | Sort-Object Name | Format-Table -AutoSize
-	
+$logListUWP = Get-AppxPackage | 
+    Select-Object Name, PackageFullName, Version | Sort-Object Name
+
+foreach ($log in $logListUWP) {
+    Write-Log $log
+}
+$logListUWP | Format-Table -AutoSize
