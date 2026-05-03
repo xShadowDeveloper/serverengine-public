fe2c14b17716940fafdb0e17af97dcf91d3636e7
xShadowDeveloper <106242550+xShadowDeveloper@users.noreply.github.com>
2026-05-03 14:45:37 +0200

    -> ServerEngine Fullsync

--- _Scripts_ServerEngine/01-SE-SaveData.ps1
+++ _Scripts_ServerEngine/01-SE-SaveData.ps1
@@ -1,6 +1,10 @@
 # You can save data to the variable $store
 # To use data on the next script using Runbooks
 #--------------------------------------------------
+function Write-Log {
+    param($Message)
+    Write-Host "<WRITE-LOG = `"*$Message*`">"
+}
 
 # Example 1 (Save Text)
 $store = "This text was saved"
@@ -12,15 +16,10 @@ $store = $message
 # Example 3 (Save Numeric Values)
 $store = 1337
 
-# Example 4 (Save Mixed Data)
-# In a loop initialze $store as array
-$store = @()
-foreach ($file in $fileList){
-	$store += $file
-}
-#or all it once if you can
-$store = @("John", 30, "Engineer", 75000.50)
+# Example 4 (Save SringList or Aarray as String)
+$store = "c.orlando, m.muster"
 
+Write-Log "Save: $store"
 #--------------------------------------------------
 # Check LoadData next for loading usage
 #--------------------------------------------------
